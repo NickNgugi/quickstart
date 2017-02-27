3ec63d4cb23512e808a999244078926209c23480
Nick <nngugi@gmail.com>
2017-02-27 14:50:16 -0500

    7. HTTP: Observables complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent } from './heroes.component';
 import { HeroService } from './hero.service';
 import { DashboardComponent } from './dashboard.component';
-
+import { HeroSearchComponent } from './hero-search.component';
 import { AppRoutingModule } from './app-routing.module'
 
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
@@ -26,6 +26,7 @@ import { InMemoryDataService } from './in-memory-data.service';
     AppComponent,
     DashboardComponent,
     HeroDetailComponent,
+    HeroSearchComponent,
     HeroesComponent
   ],
   providers: [HeroService],
