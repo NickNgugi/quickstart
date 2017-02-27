aaa209debdbc22bd0c16f6316a5e8bcdcf83321a
Nick <nngugi@gmail.com>
2017-02-27 11:47:04 -0500

    7. HTTP Delete Hero complete

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -15,16 +15,23 @@ import { HeroService } from './hero.service';
 })
 export class HeroDetailComponent implements OnInit {
   @Input() hero: Hero;
+  
   constructor(
     private heroService: HeroService,
     private route: ActivatedRoute,
     private location: Location
   ) {}
+  
   ngOnInit(): void {
     this.route.params
     .switchMap((params: Params) => this.heroService.getHero(+params['id']))
     .subscribe(hero => this.hero = hero);
   }
+  save(): void {
+    this.heroService.update(this.hero)
+      .then(()=>this.goBack());
+  }
+
   goBack(): void {
     this.location.back();
   }
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -8,6 +8,7 @@ import { Hero } from './hero';
 @Injectable()
 export class HeroService{
     private heroesUrl = 'api/heroes'; //URL to web api
+    private headers = new Headers({'Content-Type': 'application/json'});
 
     constructor(private http: Http){}
 
@@ -31,6 +32,32 @@ export class HeroService{
     //         setTimeout(() => resolve(this.getHeroes()), 2000)
     //     });
     // }
+
+    update(hero: Hero): Promise<Hero> {
+        const url = `${this.heroesUrl}/${hero.id}`;
+        return this.http
+            .put(url, JSON.stringify(hero),{headers: this.headers})
+            .toPromise()
+            .then(()=>hero)
+            .catch(this.handleError);
+    }
+
+    create(name: string): Promise<Hero> {
+        return this.http
+            .post(this.heroesUrl, JSON.stringify({name: name}), {headers: this.headers})
+            .toPromise()
+            .then(res => res.json().data)
+            .catch(this.handleError);
+    }
+
+    delete(id: number): Promise<void> {
+        const url = `${this.heroesUrl}/${id}`;
+        return this.http
+            .delete(url, {headers: this.headers})
+            .toPromise()
+            .then(() => null)
+            .catch(this.handleError);
+    }
 
     private handleError(error: any) {
         console.error('An error occured', error); //for demo purposes only
